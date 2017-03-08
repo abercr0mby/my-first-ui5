2162873cfb30051694fbe6d72093babedc825191
Paul Williams <paul.williams@menziesdistribution.com>
2017-03-08 15:48:18 +0000

    Added descriptor (manifest.json) and moved root view and i18n config here.

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,12 +2,12 @@ sap.ui.define([
    "sap/ui/core/UIComponent",
    "sap/ui/model/json/JSONModel",
    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+], function (UIComponent, JSONModel) {
    "use strict";
    return UIComponent.extend("sap.ui.demo.wt.Component", {
-		metadata : {
-			rootView: "sap.ui.demo.wt.view.App"
-		},	
+      metadata : {
+            manifest: "json"
+      },
    	
       init : function () {
          // call the init function of the parent
@@ -20,12 +20,6 @@ sap.ui.define([
          };
          var oModel = new JSONModel(oData);
          this.setModel(oModel);
-
-         // set i18n model
-         var i18nModel = new ResourceModel({
-            bundleName : "sap.ui.demo.wt.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");         
 	}
    });
 });
